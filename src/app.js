95d5216b23622f18627c706742920fb1275e08fc
Kevin Barreto <barretokevin@hotmail.com>
2021-12-13 14:44:48 -0600

    checking that new instances of classes inherit its parent classes

--- src/app.js
+++ src/app.js
@@ -1,20 +1,11 @@
-class Drone {
-  constructor(id) {
-    this._id = id; // convention for a private property with underscore (_id)
-  }
+class Vehicle {}
 
-  get id() {
-    return this._id; // we can only read, so we cannot modify with the getter.
-  }
+class Drone extends Vehicle {}
 
-  set id(newIDValue) {
-    this._id = newIDValue; // we can modify the getter value with the setter
-  }
-}
+class Car extends Vehicle {}
 
-let drone = new Drone("A123");
-console.log(drone.id);
-drone.id = "B456"; // this is the new value set in setter
-console.log(drone.id);
 
-// WE CAN ACCESS THE SETTER AND GETTERS AS BEING PROPERTIES AND NOT FUNCTIONS EVEN THOUGH THEY ARE CREATED SUCH AS A METHOD
+let c = new Car()
+console.log(c instanceof Car) // true
+console.log(c instanceof Vehicle) // true
+console.log(c instanceof Object) // true
