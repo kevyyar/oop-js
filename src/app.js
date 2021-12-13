31b64026d2a4f7cc3f2bf98da48af7907b99d611
Kevin Barreto <barretokevin@hotmail.com>
2021-12-13 13:55:17 -0600

    created instance properties inside the constructor

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 class Drone {
   constructor(id, name) {
+    //.. inside the constructor we create instance variables
+    console.log(id);
+    console.log(name);
+    //.. we use the 'this' keyword to refer to the instance being created. With this, they're being attached to the instance Drone
     this.id = id;
     this.name = name;
   }
 }
 
-Drone.maxHeight = 2000;
-
 let drone = new Drone("A123", "Flyer");
-let drone2 = new Drone("B456", "Twirl");
-
+console.log(drone.name);
 console.log(drone.id);
-console.log(drone2.id);
-console.log(drone.maxHeight); // undefined because it only exists as a class property and not an instance property
-console.log(Drone.maxHeight); // this is a class property and not an instance property so it cannot be accessed by the instance of the class
