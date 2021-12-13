d8b6a4c353bddc7146ca5c3efeaeab0d838cf30a
Kevin Barreto <barretokevin@hotmail.com>
2021-12-13 10:01:24 -0600

    class methods

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ class Drone {
     this.id = id;
     this.name = name;
   }
-}
 
-Drone.maxHeight = 2000;
+  fly() {
+    console.log("Fly this drone with ID: " + this.id);
+  }
+}
 
 let drone = new Drone("A123", "Flyer");
 let drone2 = new Drone("B456", "Twirl");
 
-console.log(drone.id);
-console.log(drone2.id);
-console.log(Drone.maxHeight);
+drone.fly();
+drone2.fly();
