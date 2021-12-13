fd04cbc53793c7a0de95cf93f8e98309fc23c850
Kevin Barreto <barretokevin@hotmail.com>
2021-12-13 14:55:35 -0600

    inheriting constructor method from parent class

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
-class Vehicle {}
+class Vehicle {
+    constructor(licenseNum) {
+        this.licenseNum = licenseNum
+    }
+}
 
 class Drone extends Vehicle {}
 
-class Car extends Vehicle {}
+class Car extends Vehicle {
+    constructor(licenseNum) {
+        super(licenseNum);
+    }
+}
 
 
-let c = new Car()
-console.log(c instanceof Car) // true
-console.log(c instanceof Vehicle) // true
-console.log(c instanceof Object) // true
+let c = new Car('License num: A123', 'Black')
+console.log(c.licenseNum)
