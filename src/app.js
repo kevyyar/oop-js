2f401c9300e3237418a4f06d3ea059d97ec8daf1
Kevin Barreto <barretokevin@hotmail.com>
2021-12-13 13:28:02 -0600

    added a comment to distinguish between a class property and an instance property

--- src/app.js
+++ src/app.js
@@ -12,4 +12,5 @@ let drone2 = new Drone("B456", "Twirl");
 
 console.log(drone.id);
 console.log(drone2.id);
-console.log(Drone.maxHeight);
+console.log(drone.maxHeight); // undefined because it only exists as a class property and not an instance property
+console.log(Drone.maxHeight); // this is a class property and not an instance property so it cannot be accessed by the instance of the class
